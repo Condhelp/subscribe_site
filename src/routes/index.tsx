0d06717375ca0b7b6b9081fe5226d4787a7dd5b1
SamuelBrunoo <samuel.k.bruno@gmail.com>
2024-10-21 15:28:08 -0300

    Incoming page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
 // Pages
-import Home from "../pages/Home"
 import Subscribe from "../pages/Subscribe"
 import WhoPage from "../pages/Who"
+import IncomingPage from "../pages/Incoming"
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<IncomingPage />} />
         <Route path="/events" element={<Subscribe />} />
         <Route path="/who" element={<WhoPage />} />
         <Route path="*" element={<Navigate to={"/"} />} />
